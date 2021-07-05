d86d58e5f9cd52dfb6c814904d6d83df225c0eca
elami <elami@DESKTOP-GF1VGP1>
2021-07-05 18:38:15 +0100

    First commit

---
+++ cinemaProject/src/test/java/org/sid/cinema/CinemaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
